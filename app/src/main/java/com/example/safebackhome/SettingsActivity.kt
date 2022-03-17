d4439cd19303314c4e7490cf54e7970beea876dd
Alex <alt1999@live.fr>
2022-03-17 19:02:38 -0400

    settings activity

---
+++ app/src/main/java/com/example/safebackhome/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.safebackhome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        editPassword= findViewById(R.id.editTextTextPassword)
+        editPin= findViewById(R.id.editTextNumberPassword)
+        alertMessage= findViewById(R.id.editAlertMessage)
+        alertMessage.setText("cc")
+    }
+    private lateinit var alertMessage: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var editPin: EditText
+}
+
